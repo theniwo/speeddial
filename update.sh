7b331ffc2f53b9b5e6b7cf129b061bd8ae8d65c0
niwo <git@theniwo.de>
2023-06-17 13:19:31 +0200

    tweaked update file

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ -z  "$*" ]; then
-	echo "Usage: $0 (-p) \"Commit description\""
+	echo "Usage: $0 \"Commit description\""
 else
 	git add . && git commit -m "$1" && git push && echo "Success" || echo "Failed"
 fi
